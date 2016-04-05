c1a09e74ea1537e08ab7e1614b0509744f8251bb
Dustin Collins <dustinrcollins@gmail.com>
2016-04-05 15:12:36 -0400

    Use policy yml

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,10 +3,10 @@ include_recipe 'build-essential'
 include_recipe 'conjur-host-identity'
 include_recipe 'conjur'
 
-template '/etc/myapp.xml' do
+template '/etc/webapp1.xml' do
   source 'myapp.xml.erb'
   variables({
-    :database_password => fetch_conjur_variable('demo/tomcat_policy/database_password'),
-    :api_token => fetch_conjur_variable('demo/tomcat_policy/api_key')
+    :database_password => fetch_conjur_variable('demo/webapp1/database_password'),
+    :api_token => fetch_conjur_variable('demo/webapp1/api_key')
   })
 end
